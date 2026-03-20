89af2964ddb57ea4d3ca00b0e00bd54a7caa2a65
vnovoid <vnovoid@gmail.com>
2026-03-20 23:24:58 +0300

    созданы 1й вариант форм регистрации,логина,  редирект на логин

--- TTK/mainapp/urls.py
+++ TTK/mainapp/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+from django.shortcuts import redirect
 from . import views
 
+app_name = "mainapp"
+
 urlpatterns = [
-    path('', views.player, name='player'),
+    path("", views.player, name="player"),
+    path("users/", include("mainapp.users.urls", namespace="users")),
 ]
